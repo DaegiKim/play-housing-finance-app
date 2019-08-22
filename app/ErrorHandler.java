d3603b8543e88a991894a4bf3519d7742445f3fc
DaegiKim <rmrhtdms@gmail.com>
2019-08-22 14:47:28 +0900

    Add LoggingFilter

--- app/ErrorHandler.java
+++ app/ErrorHandler.java
@@ -1,4 +1,6 @@
 import exceptions.FinanceRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.http.HttpErrorHandler;
 import play.mvc.Http.RequestHeader;
 import play.mvc.Result;
@@ -6,23 +8,40 @@ import play.mvc.Results;
 
 import javax.inject.Singleton;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 
 @Singleton
 public class ErrorHandler implements HttpErrorHandler {
-    public CompletionStage<Result> onClientError(
-            RequestHeader request, int statusCode, String message) {
-        return CompletableFuture.completedFuture(
-                Results.status(statusCode, "A client error occurred: " + message));
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
+    public CompletionStage<Result> onClientError(RequestHeader request, int statusCode, String message) {
+        return CompletableFuture.completedFuture(Results.status(statusCode, "A client error occurred: " + message));
     }
 
     public CompletionStage<Result> onServerError(RequestHeader request, Throwable exception) {
-        if(exception instanceof FinanceRuntimeException) {
+        logging(request, exception);
+
+        if(exception instanceof CompletionException) {
+            CompletionException completionException = (CompletionException) exception;
+            if(completionException.getCause() != null && completionException.getCause() instanceof FinanceRuntimeException) {
+                FinanceRuntimeException financeRuntimeException = (FinanceRuntimeException) completionException.getCause();
+                return CompletableFuture.completedFuture(financeRuntimeException.getResult());
+            }
+        }
+        else if(exception instanceof FinanceRuntimeException) {
             FinanceRuntimeException financeRuntimeException = (FinanceRuntimeException) exception;
             return CompletableFuture.completedFuture(financeRuntimeException.getResult());
-        } else {
-            return CompletableFuture.completedFuture(
-                    Results.internalServerError("A server error occurred: " + exception.getMessage()));
         }
+
+        return CompletableFuture.completedFuture(Results.internalServerError("A server error occurred: " + exception.getMessage()));
+    }
+
+    private void logging(RequestHeader request, Throwable exception) {
+        log.error(
+                "{} {} {}",
+                request.method(),
+                request.uri(),
+                exception.getMessage());
     }
 }
